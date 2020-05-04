bbfdb946d241acd81273a4fd6c5c9be3506734b3
landonbuell <lhb1007@wildcats.unh.edu>
2020-05-03 22:09:35 -0400

    3 May 2020
    - Updates & progress

---
+++ Chapter4/Chapter4.py
@@ -0,0 +1,15 @@
+"""
+Landon Buell
+Nueral Network Projects
+Chapter 4
+30 April 2020
+"""
+
+
+            #### IMPORTS ####
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import random
+
