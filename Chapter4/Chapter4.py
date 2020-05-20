bd4444167a9612296ad041428d1835fcf240bd4e
landonbuell <lhb1007@wildcats.unh.edu>
2020-05-19 21:19:45 -0400

    19 May 2020
    - Downloaded Data fro Chapter 4
    - Progress on chapter 4

--- Chapter4/Chapter4.py
+++ Chapter4/Chapter4.py
@@ -13,3 +13,18 @@ import matplotlib.pyplot as plt
 import os
 import random
 
+if __name__ == '__main__':
+
+    # Get list of file names
+    path = 'C:/Users/Landon/Documents/Python_Stuff/PetImages/Cat'
+    _, _, cat_images = next(os.walk(path))
+
+    # Visualize 3  x 3 images
+    fig,ax = plt.subplots(nrows=3,ncols=3,figsize=(12,12))
+
+    for idx,img in enumerate(random.sample(cat_images,9)):
+        img_read = plt.imread(path+img)
+        ax[int(idx/3),idx%3].imshow(img_read)
+        ax[int(idx/3),idx%3].axis('off')
+        ax[int(idx/3),idx%3].set_title("Cat"+img)
+    plt.show()
